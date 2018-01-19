829a794bd84dd7c8733aec2e6e1b9e4c313dcaa0
Elizabeth Veeh <elise.veeh@gmail.com>
2018-01-19 19:56:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var katzDeli = [];  
+var katzDeli = [];
 
 function takeANumber(katzDeliLine, newName){
   katzDeliLine.push(newName);
   return katzDeliLine.length;
 }
-    
+
 function nowServing(katzDeliLine){
-     
+
   return katzDeliLine.shift();
 }
