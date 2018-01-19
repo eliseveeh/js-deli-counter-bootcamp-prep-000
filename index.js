a6bc30bbcb55b81a221e838d89b63dd188936bb2
Elizabeth Veeh <elise.veeh@gmail.com>
2018-01-19 20:00:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, newName){
   katzDeliLine.push(newName);
-  return "Welcome " + newName + ". You are number " + katzDeliLine.length + "in line."
+  return "Welcome " + newName + ". You are number " + katzDeliLine.length + " in line."
 }
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length === 0){
-    return "There is nobody waiting to be served."
+    return "There is nobody waiting to be served!"
   }
   else {
     return "Currently serving " + katzDeliLine.shift() + ".";
