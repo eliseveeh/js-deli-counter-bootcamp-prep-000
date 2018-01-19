105f160ecab42216c0eb1aa3c20c6943fec88a48
Elizabeth Veeh <elise.veeh@gmail.com>
2018-01-19 20:01:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, newName){
   katzDeliLine.push(newName);
-  return "Welcome " + newName + ". You are number " + katzDeliLine.length + " in line."
+  return "Welcome, " + newName + ". You are number " + katzDeliLine.length + " in line."
 }
 
 function nowServing(katzDeliLine){
